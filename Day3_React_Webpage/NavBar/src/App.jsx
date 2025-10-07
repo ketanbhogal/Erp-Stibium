0818e0863cb41c05e87b11483bcfd5b4a01795c6
ketanbhogal <bhogalketan268@gmail.com.com>
2025-10-07 09:32:52 +0530

    Added Day3_React_Webpage with NavBar React project

---
+++ Day3_React_Webpage/NavBar/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Navbar from "./Component/Navbar";
+import "./App.css"; // import CSS
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <section className="content">
+        <h1>Welcome</h1>
+        <p></p>
+      </section>
+    </div>
+  );
+}
+
+export default App;
